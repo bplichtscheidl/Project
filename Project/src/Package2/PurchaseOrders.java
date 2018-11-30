f9223f0c509c309264af049205540cb02b0a68c5
ZachBrier <zachbrier1@gmail.com>
2018-11-30 13:54:36 -0600

    Re-added Code to PurchaseOrders Class

--- Project/src/Package2/PurchaseOrders.java
+++ Project/src/Package2/PurchaseOrders.java
@@ -1,7 +1,44 @@
 package Package2;
+import java.util.*;
+
 import Package1.*;
 
 public class PurchaseOrders {
+	private Date date;
+	private String estArrivalTime;
+	private int ordSize;
 	
+	public PurchaseOrders() {
+		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getEstArrivalTime() {
+		return estArrivalTime;
+	}
+
+	public void setEstArrivalTime(String estArrivalTime) {
+		this.estArrivalTime = estArrivalTime;
+	}
+
+	public int getOrdSize() {
+		return ordSize;
+	}
+
+	public void setOrdSize(int ordSize) {
+		this.ordSize = ordSize;
+	}
+	
+	public String toString() {
+		String str = " ";
+		return str;
+	}
 
 }
